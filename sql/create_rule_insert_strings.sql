b193a7c9bfbbfa3846c7292592af03a1ce66bfb1
donbro <donbro.x@gmail.com>
2013-06-25 00:41:20 -0400

    efforts towards understanding the rules mechanism

--- sql/create_rule_insert_strings.sql
+++ sql/create_rule_insert_strings.sql
@@ -6,19 +6,34 @@
 
 drop rule if exists insert_strings on strings;
 
+/*
+    For ON INSERT rules, the original query (if not suppressed by INSTEAD) is done before any actions added by rules. 
+ */
+
 CREATE RULE insert_strings AS 
 ON INSERT TO strings 
+
+
+/* this qualification turns the INSERT on or off
+    if the row exists or not.  Thus the duplicate entry exception
+    never happens. */
 
 WHERE (
     EXISTS (
         SELECT 1 FROM strings WHERE (
-                (strings.string)::text = (new.string)::text
+                (strings.string)::text = (new.string)::text and hey_there() 
                 )
             )
       ) 
 
-DO INSTEAD 
 
+/* if we did "where true" here, the insert would never happen 
+    and for a new insertion the select would not find a matching row. */
+DO INSTEAD 
+/* if this whole rule is "thrown into" the rewrite system 
+    again, ie after the insert, then, afterwards, the exists 
+    would be true, which is what we see. */
+(
 SELECT 
     strings.str_id , 
     strings.string,
@@ -27,5 +42,11 @@ SELECT
 FROM 
     strings 
 WHERE ((strings.string)::text = (new.string)::text);
+--select 'hi' as groink;
+)
+
+/* at this point, "new" hasn't been through the trigger,
+    so get the already inserted record(s) from the table
+    which have been through the trigger. */
 
 
--- sql/create_trigger_before_insert_strings.sql
+++ sql/create_trigger_before_insert_strings.sql
@@ -1,7 +1,7 @@
 /* "create_trigger_before_insert_strings.sql" */
 
 
-CREATE FUNCTION trigger_before_insert_strings() RETURNS trigger
+CREATE or replace FUNCTION trigger_before_insert_strings() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 BEGIN
@@ -9,48 +9,37 @@ BEGIN
 /* scheme for string ids is: "str000000" or  'str[0-9]{6}' */
 
 --debugging output:
--- new.file_name := 'z' || new.string_id || 'K' || new.string_id is null || ((NEW.string_id NOT SIMILAR TO 'str[0-9]{6}'));
+-- new.file_name := 'z' || new.str_id || 'K' || new.str_id is null || ((NEW.str_id NOT SIMILAR TO 'str[0-9]{6}'));
 
-IF new.string_id is null or (NEW.string_id NOT SIMILAR TO 'str[0-9]{6}') then
+IF new.str_id is null or (NEW.str_id NOT SIMILAR TO 'str[0-9]{6}') then
 
-        new.string_id := 
+        new.str_id := 
                 concat( 'str' , 
-                    right( concat( '0000000' , (1 + (select substring( coalesce( max(string_id), 'str000000' )  from 4 for 6) from strings )::integer)::text ) , 6)
+                    right( concat( '0000000' , (1 + (select substring( coalesce( max(str_id), 'str000000' )  from 4 for 6) from strings )::integer)::text ) , 6)
                     )
         ;
 
 end if;
 
-new.string_ts := to_tsvector('english', new.string);
+new.str_ts := to_tsvector('english', new.string);
 
 RETURN new; 
 
 /*
-  new.string_ts :=
+  new.str_ts :=
      setweight(to_tsvector('pg_catalog.english', coalesce(new.title,'')), 'A') ||
      setweight(to_tsvector('pg_catalog.english', coalesce(new.body,'')), 'D');
 */
 
 END;
 
---BEGIN
---    IF EXISTS (SELECT 1 FROM "myschema".mytable WHERE "MyKey" = NEW."MyKey") THEN
---        RETURN NULL;
---    ELSE
---        RETURN NEW;
---    END IF;
---END;
-
---CREATE RULE "my_table_on_duplicate_ignore" AS ON INSERT TO "my_table"
---  WHERE EXISTS(SELECT 1 FROM my_table 
---                WHERE (pk_col_1, pk_col_2)=(NEW.pk_col_1, NEW.pk_col_2))
---  DO INSTEAD NOTHING;
-
 $$;
 
 
 ALTER FUNCTION public.trigger_before_insert_strings() OWNER TO donb;
 
+
+drop trigger before_insert_strings on strings;
 
 CREATE TRIGGER before_insert_strings BEFORE INSERT ON strings FOR EACH ROW EXECUTE PROCEDURE trigger_before_insert_strings();
 
