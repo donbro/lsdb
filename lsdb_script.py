9419d6469147f1b520e273ffff34d06a86f79d85
donbro <donbro.x@gmail.com>
2020-02-13 23:40:38 -0600

    new era cleanup

--- lsdb_script.py
+++
@@ -1,9 +0,0 @@
-#!/Users/donb/projects/VENV/mysql-connector-python/bin/python
-# EASY-INSTALL-ENTRY-SCRIPT: 'lsdb==0.5','console_scripts','lsdb'
-__requires__ = 'lsdb==0.5'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('lsdb==0.5', 'console_scripts', 'lsdb')()
-)
