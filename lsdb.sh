1461939cf21427961058a6eae965469fa9238ad4
donbro <donbro.x@gmail.com>
2022-05-30 21:42:59 -0500

    lsdb.sh to csv

--- lsdb.sh
+++ lsdb.sh
@@ -12,7 +12,7 @@
 function lsdb() { 
 
 	findpath=$( /Users/donb/coreutils-9.0/src/realpath "$1" )
-	echo "   findpath == "$findpath
+	#echo "   findpath == "$findpath
 	
 	#/Users/donb/coreutils-9.0/src/realpath -z "$1" # -z is no trailing CR
 
@@ -20,23 +20,23 @@ function lsdb() {
         case "$uname" in
         Darwin) #echo "uname == Darwin"
 		VOL=$(df "$1" | grep -v "^Filesystem" | cut -f 1 -d " ")
-		echo "        VOL == "$VOL
+		#echo "        VOL == "$VOL
 		VOLNAME=$(diskutil info "$VOL" | grep  "Volume Name" | cut -b 30- )
-		echo "    VOLNAME == "$VOLNAME
+		#echo "    VOLNAME == "$VOLNAME
 		VOLUUID=$(diskutil info $VOL | grep "Volume UUID" | awk '{print $3}')
-		echo "    VOLUUID" "==" $VOLUUID
+		#echo "    VOLUUID" "==" $VOLUUID
 
 		if [ -z "${MACHINEID}" ]; then #  "MACHINEID is unset or set to the empty string"
 	    	    MACHINEID=$( system_profiler SPHardwareDataType  | grep -e "Hardware UUID:" | \
 				cut -b 22- )
-		    echo "  MACHINEID" "<=" $MACHINEID
-		else # non-empty string 
-		    echo "  MACHINEID" "==" $MACHINEID
+		    #echo "  MACHINEID" "<=" $MACHINEID
+		#else # non-empty string 
+		    #echo "  MACHINEID" "==" $MACHINEID
 		fi
 
 
-		findpathhash=$( md5 -q "$findpath")
-		echo "findpathhash" "==" $findpathhash
+		#findpathhash=$( md5 -q "$findpath")
+		#echo "findpathhash" "==" $findpathhash
            	;;
         FreeBSD) echo "$uname = FreeBSD" 
            ;;
@@ -51,50 +51,37 @@ function lsdb() {
 
 
 
-	echo "  PROCESSID" "==" $$
+	PROCESSID=$$
+	#echo "PROCESSID" "==" $PROCESSID
+
 
 	DATETIMEUTC=$(python2.7 -c "import datetime ; \
 			print datetime.datetime.utcnow().isoformat()")
-
-	echo "DATETIMEUTC" "==" $DATETIMEUTC
+	#echo "DATETIMEUTC" "==" $DATETIMEUTC
 
 	# find => stat => awk
-	printf "VOLUME UUID,Inode Number,total size (bytes),last access (seconds),data mod (seconds),status change (seconds),creation (seconds),file type,file path\n\n"
+	#printf "VOLUME UUID,Inode Number,total size (bytes),last access (seconds),data mod (seconds),status change (seconds),creation (seconds),file type,file path\n\n"
 
 	# stat offers different c style quoting, eg
 	#	'/Users/donb/test_files/'\''single "quoted"'\''.txt'
 	#	'/Users/donb/test_files/embedded'$'\026\n''return.txt'
 	#	'/Users/donb/test_files/return'$'\016''embedded.txt'
 	#  but none of these are CSV-style, double quotes around doubled-double quotes (if inside)
+	#  gnu stat can terminate (its output of) a record with \0
 
 	find "$findpath" \
 		-print0 | \
 	xargs -0 -I {} /Users/donb/coreutils-9.0/src/stat  \
-		--printf=$VOLUUID,'%i,%s,%X,%Y,%Z,%W,%F,%n\n\n' "{}" 
-
-return
-
-	echo
-
-        find "$1"  \
-		-print0 | \
-	xargs -0 -I {} /Users/donb/coreutils-9.0/src/realpath -z "{}" | \
-	xargs -0 -I {} /Users/donb/coreutils-9.0/src/stat \
-		--printf="$VOLUUID,%i,%s,%X,%Y,%Z,%W,%F,%n\0" \
-		"{}" | \
-	gawk -v VID=$VOLUUID 'BEGIN { RS="\0"; FS=","; OFS=" -- "  } \
+		--printf='%i,%s,%X,%Y,%Z,%W,%F,%n\0' "{}" | \
+	gawk -v VID=$VOLUUID -v MID=$MACHINEID -v PID=$PROCESSID -v TID=$DATETIMEUTC \
+		'BEGIN { RS="\0"; FS=","; OFS=","  } \
         	{
-        	ll=length($0)
-        	XX=9+length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)
+        	XX=1+length($1)+1+length($2)+1+length($3)+1+length($4)+1+length($5)+1+length($6)+1+length($7)+1
         	SS=substr($0,XX)
-        	ZZ=gsub("\"","\"\"",SS)
-
-		"md5 -q "SS | getline MM
-		print "MM", MM
-
-
-        	print VID,MM,ll,XX,$1,$2,$3,$4,$5,$6,$7,"\""$8"\"","<<"SS">>";
+        	gsub("\"","\"\"",SS) # substitute "in place"
+        	print MID,TID,PID,VID,$1,$2,$3,$4,$5,$6,$7,"\""SS"\"";
         	}'
+
 
 }
 
@@ -113,3 +100,26 @@ return
 	# 	(gnu) stat        --printf=FORMAT
         #              like --format, but interpret backslash escapes, and do not
         #              output a mandatory trailing newline;
+
+	#echo
+
+        #find "$1"  \
+		#-print0 | \
+	#xargs -0 -I {} /Users/donb/coreutils-9.0/src/realpath -z "{}" | \
+	#xargs -0 -I {} /Users/donb/coreutils-9.0/src/stat \
+		#--printf="$VOLUUID,%i,%s,%X,%Y,%Z,%W,%F,%n\0" \
+		#"{}" | \
+	#gawk -v VID=$VOLUUID  MID=$MACHINEID\
+		#'BEGIN { RS="\0"; FS=","; OFS=" -- "  } \
+        	#{
+        	#ll=length($0)
+        	#XX=9+length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)
+        	#SS=substr($0,XX)
+        	#ZZ=gsub("\"","\"\"",SS)
+
+		#"md5 -q "SS | getline MM
+		#print "MM", MM
+#
+
+        	#print VID,MM,ll,XX,$1,$2,$3,$4,$5,$6,$7,"\""$8"\"","<<"SS">>";
+        	#}'
